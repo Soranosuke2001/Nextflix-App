addec41f12aab9d23bacefdc4d1ae0f99e437cd0
sora <sorazora1.4@gmail.com>
2023-04-08 15:44:39 -0700

    added loading feature when being redirected from login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,44 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { mClient } from '@/lib/magic-client';
+
+import Loading from '@/components/loading/loading';
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const loggedIn = await mClient.user.isLoggedIn();
+
+      if (loggedIn) {
+        router.push('/');
+      } else {
+        router.push('/login');
+      }
+    };  
+
+    checkLogin();
+  }, [])
+
+  useEffect(() => {
+    const loadingComplete = () => {
+      setLoading(false);
+    };
+
+    router.events.on("routeChangeComplete", loadingComplete);
+    router.events.on("routeChangeError", loadingComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", loadingComplete);
+      router.events.off("routeChangeError", loadingComplete);
+    };
+  }, [router]);
+
+  return (
+    loading ? <Loading /> : <Component {...pageProps} />
+  );
+};
