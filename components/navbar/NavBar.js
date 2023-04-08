ceb6a5a40e0a5713ddebd89bac8a33e893187f6b
sora <sorazora1.4@gmail.com>
2023-04-08 04:56:04 -0700

    adjusted some code to position dropdown menu

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -8,26 +8,26 @@ const NavBar = (props) => {
         <a className={styles.logoLink} href="/">
           <div className={styles.logoWrapper}>Netflix</div>
         </a>
-      </div>
-      <ul className={styles.navItems}>
-        <li className={styles.navItem}>Home</li>
-        <li className={styles.navItem2}>My List</li>
-      </ul>
+        <ul className={styles.navItems}>
+          <li className={styles.navItem}>Home</li>
+          <li className={styles.navItem2}>My List</li>
+        </ul>
 
-      <nav className={styles.navContainer}>
-        <div>
-          <button className={styles.usernameBtn}>
-            <p className={styles.username}>{username}</p>
-          </button>
-          {/* Expand more icons */}
-          <div className={styles.navDropdown}>
-            <div>
-              <a className={styles.linkName}>Sign Out</a>
-              <div className={styles.lineWrapper}></div>
+        <nav className={styles.navContainer}>
+          <div>
+            <button className={styles.usernameBtn}>
+              <p className={styles.username}>{username}</p>
+            </button>
+            {/* Expand more icons */}
+            <div className={styles.navDropdown}>
+              <div>
+                <a className={styles.linkName}>Sign Out</a>
+                <div className={styles.lineWrapper}></div>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
