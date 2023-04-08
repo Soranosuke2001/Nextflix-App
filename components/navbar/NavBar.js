d8ae08c21d4da24c5c9751e86793b1bbaf5f448b
sora <sorazora1.4@gmail.com>
2023-04-08 04:51:09 -0700

    added styling to the navbar but not fully complete

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,25 +1,35 @@
-const NavBar = (props) => {
-    const { username } = props;
-    return (
-        <div>
-            <p>NavBar Component</p>
-            <ul>
-                <li>Home</li>
-                <li>My List</li>
-            </ul>
+import styles from "./NavBar.module.css";
 
-            <nav>
-                <div>
-                <button>
-                    <p>{username}</p>
-                </button>
-                <div>
-                    <a>Sign Out</a>
-                </div>
-                </div>
-            </nav>
+const NavBar = (props) => {
+  const { username } = props;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <a className={styles.logoLink} href="/">
+          <div className={styles.logoWrapper}>Netflix</div>
+        </a>
+      </div>
+      <ul className={styles.navItems}>
+        <li className={styles.navItem}>Home</li>
+        <li className={styles.navItem2}>My List</li>
+      </ul>
+
+      <nav className={styles.navContainer}>
+        <div>
+          <button className={styles.usernameBtn}>
+            <p className={styles.username}>{username}</p>
+          </button>
+          {/* Expand more icons */}
+          <div className={styles.navDropdown}>
+            <div>
+              <a className={styles.linkName}>Sign Out</a>
+              <div className={styles.lineWrapper}></div>
+            </div>
+          </div>
         </div>
-    );
+      </nav>
+    </div>
+  );
 };
 
 export default NavBar;
