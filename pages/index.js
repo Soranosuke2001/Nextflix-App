4ff0303935cc381554ff4f7bac0d760ce231eae9
sora <sorazora1.4@gmail.com>
2023-04-08 14:39:16 -0700

    modified index.js homepage

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <NavBar username="Sora" auth="false" />
+      <NavBar />
       <Banner
         title="Kamisato Ayaka"
         subTitle="Hoyoverse"
