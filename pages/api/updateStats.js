bc88405c9d77c390794fa86b00f17e39b6bf1af9
Sora <sorazora1.4@gmail.com>
2023-04-09 18:06:32 -0700

    added more error handling

--- pages/api/updateStats.js
+++ pages/api/updateStats.js
@@ -1,37 +1,54 @@
-import { fetchVideoStats, insertStats, updateStats } from '@/lib/db/hasura';
-import jwt from 'jsonwebtoken';
+import { fetchVideoStats, insertStats, updateStats } from "@/lib/db/hasura";
+import jwt from "jsonwebtoken";
 
 const updateState = async (req, res) => {
-    if (req.method === 'POST') {
-        try {
-            const jwtToken = req.cookies.token;
-            if (jwtToken) {
-                const tokenDecoded = jwt.verify(jwtToken, process.env.HASURA_JWT_SECRET_KEY);
+  if (req.method === "POST") {
+    try {
+      const jwtToken = req.cookies.token;
+      if (jwtToken) {
+        const tokenDecoded = jwt.verify(
+          jwtToken,
+          process.env.HASURA_JWT_SECRET_KEY
+        );
 
-                const userId = tokenDecoded.issuer;
-                const videoId = req.query.videoId;
+        const userId = tokenDecoded.issuer;
+        const videoId = req.query.videoId;
 
-                const { watched, favourited } = req.body;
+        const { watched = true, favourited } = req.body;
 
-                // returns true if the video exists in the stats table
-                const checkStatsQuery = await fetchVideoStats(userId, videoId, jwtToken);
+        if (videoId) {
+          // returns true if the video exists in the stats table
+          const checkStatsQuery = await fetchVideoStats(
+            userId,
+            videoId,
+            jwtToken
+          );
 
-                if (checkStatsQuery) {
-                    const response = await updateStats({ userId, videoId: "63bUBEIhpNk", watched, favourited }, jwtToken);
-                    res.json({ message: 'working', response: checkStatsQuery });
-                } else {
-                    const response = await insertStats({ userId, videoId: "63bUBEIhpNk", watched, favourited }, jwtToken)
-                    res.json({ message: 'working', response: checkStatsQuery });
-                }
-            } else {
-                res.status(403).json({ message: 'You do not have the correct permissions' });
-            }
-
-        } catch (error) {
-            res.status(500).json({ message: 'Error updating the database', error});
-        };  
-    };
+          if (checkStatsQuery) {
+            const response = await updateStats(
+              { userId, videoId: "63bUBEIhpNk", watched, favourited },
+              jwtToken
+            );
+            res.json({ message: "working", response: checkStatsQuery });
+          } else {
+            const response = await insertStats(
+              { userId, videoId: "63bUBEIhpNk", watched, favourited },
+              jwtToken
+            );
+            res.json({ message: "working", response: checkStatsQuery });
+          }
+        } else {
+          res
+            .status(403)
+            .json({ message: "You do not have the correct permissions" });
+        }
+      } else {
+        res.status(400).json({ message: "Video ID was missing" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Error updating the database", error });
+    }
+  }
 };
 
 export default updateState;
-
