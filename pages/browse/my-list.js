e0e6388b14cfa10a755c695ea17747c0b1a3e6f6
sora <sorazora1.4@gmail.com>
2023-04-09 21:23:05 -0700

    now renders the list of liked videos on MyList page

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -4,22 +4,40 @@ import Head from "next/head";
 import NavBar from "@/components/navbar/NavBar";
 import SectionCards from "@/components/card/SectionCards";
 
-import styles from '@/styles/MyList.module.css';
+import styles from "@/styles/MyList.module.css";
+import { getMyListVideos } from "@/lib/videos";
+import useRedirectUser from "@/util/redirect";
 
-const MyList = () => {
-    return (
-        <React.Fragment>
-            <Head >
-                <title>My List</title>
-            </Head>
-            <main className={styles.main}>
-                <NavBar />
-            </main>
-            <div className={styles.sectionWrapper}>
-                <SectionCards title="My List" videos={[]} size="small" />
-            </div>
-        </React.Fragment>
-    );
+export const getServerSideProps = async (context) => {
+  const { userId, jwtToken, redirect = null } = await useRedirectUser(context);
+
+  if (redirect) {
+    return { redirect };
+  }
+  ("");
+
+  const watchedVideos = await getMyListVideos(userId, jwtToken);
+  return {
+    props: {
+      watchedVideos,
+    },
+  };
+};
+
+const MyList = ({ watchedVideos }) => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My List</title>
+      </Head>
+      <main className={styles.main}>
+        <NavBar />
+      </main>
+      <div className={styles.sectionWrapper}>
+        <SectionCards title="My List" videos={ watchedVideos } size="small" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default MyList;
